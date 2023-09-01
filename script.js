2d53195e7761525bde815ae27761685ed112e933
Dev Chauhan <devccode8@gmail.com>
2023-09-01 13:54:55 +0530

    first commit4

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyNq9G5mzLMIhDX12dVPs42tIJTzKX6c35zhOYEqILGLrmdAnkMhhWmX7yY4AvhxmXTxQ/exec'
+const form = document.forms['submit-to-google-sheet']
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+.then(response => {
+    alert("Registred")
+    console.log('Success!', response)
+})
+.catch(error => console.error('Error!', error.message))
+})
